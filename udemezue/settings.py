8a5575db1236971e6f5176a50ad107129e28e772
Nwabufor John <johnzeus14@gmail.com>
2021-07-29 14:10:59 +0100

    Initial commit

--- udemezue/settings.py
+++ udemezue/settings.py
@@ -28,8 +28,8 @@ DEBUG = True
 
 ALLOWED_HOSTS = [
 
- 'herokudjangoapp.herokuapp.com',
- '*',
+ 'udemezue.herokuapp.com',
+
 ]
 
 
@@ -174,17 +174,17 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
-PROJECT_ROOT   =   os.path.join(os.path.abspath(__file__))
-STATIC_ROOT  =   os.path.join(PROJECT_ROOT, 'staticfiles')
+
+STATIC_ROOT  =   os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 # Extra lookup directories for collectstatic to find static files
 STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, 'static'),
+     os.path.join(BASE_DIR, 'static'),
 )
 
 #  Add configuration for static files storage using whitenoise
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
 # The database configuration.
