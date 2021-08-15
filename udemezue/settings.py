51e96c1bbe5d5b3d3c35fff84910ab02ee8c588f
Nwabufor John <johnzeus14@gmail.com>
2021-08-15 20:55:23 +0100

    updated settings

--- udemezue/settings.py
+++ udemezue/settings.py
@@ -200,9 +200,9 @@ STATIC_URL = '/static/'
 STATIC_ROOT  =   os.path.join(BASE_DIR, 'staticfiles')
 
 
-MEDIA_URL = '/media/'
+# MEDIA_URL = '/media/'
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+# MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Extra lookup directories for collectstatic to find static files
 STATICFILES_DIRS = (
