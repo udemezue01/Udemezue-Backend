93bdfed18879e1d0a868cae341c4d2418ceb92a9
Nwabufor John <johnzeus14@gmail.com>
2021-08-15 21:07:29 +0100

    updated settings

--- udemezue/settings.py
+++ udemezue/settings.py
@@ -52,6 +52,8 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'whitenoise.middleware.WhiteNoiseMiddleware',
+
+    
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -205,11 +207,11 @@ STATIC_ROOT  =   os.path.join(BASE_DIR, 'staticfiles')
 # MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Extra lookup directories for collectstatic to find static files
-STATICFILES_DIRS = (
+# STATICFILES_DIRS = (
 
-    os.path.join(BASE_DIR, 'static'),
+#     os.path.join(BASE_DIR, 'static'),
 
-    )
+#     )
 
 #  Add configuration for static files storage using whitenoise
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
