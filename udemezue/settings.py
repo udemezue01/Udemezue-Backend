43423b3b31be4c4e342b4b90e0d0df3e3cb37854
Nwabufor John <johnzeus14@gmail.com>
2022-01-28 14:32:26 +0100

    Updated media files

--- udemezue/settings.py
+++ udemezue/settings.py
@@ -101,7 +101,6 @@ DATABASES={
             'ENGINE':'django.db.backends.sqlite3',
             'NAME': BASE_DIR /'db.sqlite3',
 
-
         }
     }
 
@@ -190,12 +189,12 @@ CLOUDINARY_STORAGE = {
     'API_SECRET': 'QQhgkMFnNrGn63ibodA5kLfHNDc'
 }
 
-MEDIA_URL = '/media/'  # or any prefix you choose
+MEDIA_URL = '/media/'  
 
-# MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 STATIC_URL = '/static/'
-# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # STATICFILES_DIRS = (
 
