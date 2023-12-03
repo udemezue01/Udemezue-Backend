ec324c6fcd20b69ff34b41dfe87b8d33303c01ae
Udemezue John <johnzeus14@gmail.com>
2023-12-03 14:47:04 +0100

    modified code

--- portfolio/models.py
+++ portfolio/models.py
@@ -8,7 +8,7 @@ class Portfolio(models.Model):
 	user 			=  models.ForeignKey(settings.AUTH_USER_MODEL, on_delete = models.CASCADE, null = True)
 	title 			=  models.CharField(max_length = 3000)
 	about 			=  models.CharField(max_length = 3000)
-	image			=  models.ImageField(upload_to='images/', blank = True)
+	image			=  models.ImageField(blank = True, null = True)
 
 	def __str__(self):
 		return self.title
